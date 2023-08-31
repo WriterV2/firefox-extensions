71fc9f52089bb72fb3385a994f691d2cfc75e8cf
WriterV2 <primary-v2@protonmail.com>
2023-08-31 10:28:54 +0200

    Add video placeholder to disabled images

--- disable_wwenetwork_images/disable_wwenetwork_images.js
+++ disable_wwenetwork_images/disable_wwenetwork_images.js
@@ -1,8 +1,14 @@
 setInterval(() => {
     main();
-}, 2000);
+}, 100);
 
 function main() {
+
+    let placeholders = document.getElementsByClassName("placeholder");
+    for (let i = 0; i < placeholders.length; i++) {
+        placeholders[i].setAttribute("style", "");
+    }
+
     let card_backgrounds = document.getElementsByClassName("card-background");
     for (let i = 0; i < card_backgrounds.length; i++) {
         card_backgrounds[i].firstChild.setAttribute("src", "");
